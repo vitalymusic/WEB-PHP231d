da80e22b99e3b8730b9e0f6119d2d8a7e3a3cfb0
Vitālijs KOrabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2023-05-22 21:52:14 +0300

    updated

--- upload.php
+++ upload.php
@@ -1,10 +1,10 @@
 <?php 
-
+var_dump($_FILES);
 if($_POST){
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";}
+    echo "The file ". htmlspecialchars( basename( $_FILES["file"]["name"])). " has been uploaded.";}
 }
 ?>
 
